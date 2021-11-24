19701df952abe9f667089e0a17891f350230d0dc
AzureAD\박준형 <yoseb95@gmail.com>
2021-11-24 11:35:08 +0900

    first commit

---
+++ 소스.cpp
@@ -0,0 +1,21 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int factorial(int num);
+
+int main() {
+	int input = 0;
+	printf("숫자를 입력하면 팩토리얼을 계산합니다.\n");
+	scanf("%d", &input);
+	printf("%d팩토리얼의 결과는 %d입니다.\n", input,factorial(input));
+
+	return 0;
+};
+
+int factorial(int num) {
+	int result = 1;
+	for (int i = num; i > 0;  i--) {
+		result *= i;
+	};
+	return result;
+};
